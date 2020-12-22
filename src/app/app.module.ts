5dff82cc713040da1fdc89dc4eaba9bebf14f397
Matcarro <matcar_2001@libero.it>
2020-12-22 12:47:14 +0100

    funziona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MapComponent
+
+    MapComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    LeafletModule
+    LeafletModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ɵINTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS } from '@angular/platform-browser-dynamic';
 import * as L from 'leaflet';
+import { HttpClient } from '@angular/common/http';
+import {GeoJsonObject} from "geojson";
 
 @Component({
+
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css'],
@@ -10,28 +14,49 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   bounds1= L.latLng(38.558, 11.579)
   bounds2= L.latLng(38.558, 11.579)
+  map: L.Map
+  options
 
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  ngAfterViewInit(): void {}
+  constructor(private http: HttpClient) {
 
 
 
-  options = {
-    layers: [
-      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 6, minZoom: 6, attribution: '...' })
-    ],
-    minZoom: 6,
-    maxZoom: 6,
-    zoom: 6,
-    center: L.latLng(38.558, 11.579),
-    maxBounds: L.latLngBounds(this.bounds1, this.bounds2)
-  };
+  }
 
+  ngOnInit(): void {
 
+    //this.map=L.map("map")
+    this.options = {
+      layers: [
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 6, minZoom: 6, attribution: '...' }),
 
+      ],
+      minZoom: 6,
+      maxZoom: 6,
+      zoom: 6,
+      center: L.latLng(38.558, 11.579),
+      maxBounds: L.latLngBounds(this.bounds1, this.bounds2)
 
+    }
+  }
 
+  ngAfterViewInit(): void {
+
+  }
+
+  onMapReady(map: L.Map) {
+    this.http
+      .get(
+      'https://raw.githubusercontent.com/openpolis/geojson-italy/master/geojson/limits_IT_regions.geojson'
+      )
+      .subscribe((res: GeoJsonObject) => {
+        L.geoJSON(res, {
+        style: function (features) {
+          {
+            return {color: "red"}
+          }
+        }
+        }).addTo(map);
+      });
+  }
 }
